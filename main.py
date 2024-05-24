e0b0f03b62c870dced9ec77571244580d4d6ca94
giufalcao <thegiuliacavalcanti@gmail.com>
2024-05-24 18:06:29 -0300

    - finishing data ingestion

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
-from config.logging import logger
+from src.constants import constants
+from preprocess.preprocess import DataIngestionTrainingPipeline
+from src.logging import logger
 
-logger.info("Starting Chest Cancer Detection")
+
+if __name__ == '__main__':
+    try:
+        logger.info(f">>>>>> Stage {constants.STAGE_NAME} started <<<<<<")
+        obj = DataIngestionTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> Stage {constants.STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
